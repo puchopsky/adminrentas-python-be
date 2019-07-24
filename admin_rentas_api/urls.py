57407a0f218eceac08d91f5a96686fb4c90b92cf
Rafael Augusto Galvan Fuentes <rgalvan@tiempodevelopment.com>
2019-07-24 17:59:09 -0500

    Added the JWT token, still a WIp

--- admin_rentas_api/urls.py
+++ admin_rentas_api/urls.py
@@ -2,10 +2,19 @@
 from django.urls import path, include
 from .controller_views.OwnedPropertyView import OwnedPropertyView
 from rest_framework import routers
+from django.conf.urls import url
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 router = routers.DefaultRouter()
 router.register('property', OwnedPropertyView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    url(r'token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'token/verify/$', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- adminrentas/settings.py
+++ adminrentas/settings.py
@@ -131,6 +131,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
